99763338288693b5b4670dbdaa175dd29acd9229
Elliot Thomas <eth0net@pm.me>
2024-07-18 02:21:00 +0100

    no colony leader thought

--- 1.5/Source/GameComponent_ColonyLeadership.cs
+++ 1.5/Source/GameComponent_ColonyLeadership.cs
@@ -6,13 +6,15 @@ namespace ColonyLeadership;
 
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 [SuppressMessage("ReSharper", "InconsistentNaming")]
-public class GameComponent_ColonyLeadership : GameComponent
+public class GameComponent_ColonyLeadership(Game game) : GameComponent
 {
-    private Pawn _leader;
+    private const int TicksWithoutLeaderForThought = GenDate.TicksPerQuadrum;
 
-    public GameComponent_ColonyLeadership(Game game)
-    {
-    }
+    private readonly TickManager _tickManager = game.tickManager;
+
+    private int _lastTickWithActiveLeader;
+
+    private Pawn _leader;
 
     public Pawn Leader
     {
@@ -25,6 +27,22 @@ public class GameComponent_ColonyLeadership : GameComponent
             _leader?.needs.mood.thoughts.memories.TryGainMemory(
                 ThoughtMaker.MakeThought(ThoughtDefOf.ColonyLeadershipGained, 0));
         }
+    }
+
+    private bool HasActiveLeader => _leader is { Dead: false, Downed: false, IsPrisoner: false, IsSlave: false };
+
+    public bool ThoughtActive => _tickManager.TicksGame - _lastTickWithActiveLeader >= TicksWithoutLeaderForThought;
+
+    public override void GameComponentTick()
+    {
+        base.GameComponentTick();
+
+        // Limit to every 60 ticks to reduce performance impact
+        // Offset by a few ticks as 60 ticks is probably popular
+        if (!GenTicks.IsTickInterval(5, 60)) return;
+
+        // If there is an active leader, update the last tick with an active leader
+        if (HasActiveLeader) _lastTickWithActiveLeader = _tickManager.TicksGame;
     }
 
     public override void ExposeData()
---
+++ 1.5/Source/ThoughtWorker_NoColonyLeader.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using RimWorld;
+using Verse;
+
+namespace ColonyLeadership;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+public class ThoughtWorker_NoColonyLeader : ThoughtWorker
+{
+    public override ThoughtState CurrentStateInternal(Pawn p)
+    {
+        if (p.Faction is { IsPlayer: false }) return false;
+
+        var comp = Current.Game.GetComponent<GameComponent_ColonyLeadership>();
+
+        return comp.ThoughtActive;
+    }
+}
