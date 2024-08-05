bf64e32c3546aa64967ef787f86a3ac49e0c5ba2
Elliot Thomas <eth0net@pm.me>
2024-08-05 22:45:54 +0100

    change trade price improvement to negotiation

--- 1.5/Source/CompColonyLeader.cs
+++ 1.5/Source/CompColonyLeader.cs
@@ -43,20 +43,3 @@ public class CompColonyLeader : ThingComp
         }
     }
 }
-
-[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
-[SuppressMessage("ReSharper", "InconsistentNaming")]
-[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
-public class CompProperties_ColonyLeader : CompProperties
-{
-    public readonly List<StatModifier> statFactors =
-    [
-        new StatModifier { stat = StatDefOf.SocialImpact, value = 1.25f },
-        new StatModifier { stat = StatDefOf.TradePriceImprovement, value = 1.25f }
-    ];
-
-    public CompProperties_ColonyLeader()
-    {
-        compClass = typeof(CompColonyLeader);
-    }
-}
